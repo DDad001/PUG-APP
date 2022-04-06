1f9b1c000560f0a2ac02d063e0326a5a05e49a72
Jesse <JesseAbarca888@gmail.com>
2022-04-05 22:25:56 -0700

    Basic layout of CreateAccountScreen!

---
+++ Screens/CreateAccountScreen.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
+
+const CreateAccountScreen: FC = () => {
+    return(
+        <View>
+            <Text>I am create Account Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default CreateAccountScreen;
