7956a629a15ae313c4e9e08d97b5af6f806a59eb
Jesse <JesseAbarca888@gmail.com>
2022-04-05 18:58:43 -0700

    Imported the Pug Opening Screen to the main file

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PugOpeningScreen from './Screens/PugOpeningScreen';
 
 export default function App() {
   return (
