cb99620d9b780e2dd7ae66e7212a3e766c28ae29
Jesse <JesseAbarca888@gmail.com>
2022-04-05 23:30:50 -0700

    Named a page the wrong title!

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //Screens
-import PugOpeningScreen from './Screens/PugOpeningScreen';
 
 //Components
 import PUGbutton from './Components/PUGButton';
--- Screens/UploadFirstEventScreen.tsx
+++ Screens/UploadFirstEventScreen.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const PugOpeningScreen: FC = () => {
+const UploadFirstEventScreen: FC = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.Titletxt}>PUG</Text>
@@ -31,4 +31,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default PugOpeningScreen;
+export default UploadFirstEventScreen;
