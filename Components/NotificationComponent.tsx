b9f531a8959a189b84deb3fe309694900610e2a4
Scotterpop73 <sctzone@aol.com>
2022-04-11 11:17:48 -0700

    Completed Notifcations Screen and Compnent

--- Components/NotificationComponent.tsx
+++ Components/NotificationComponent.tsx
@@ -1,10 +1,80 @@
 import { FC } from "react";
-import { Text, ScrollView, StyleSheet, Image } from "react-native";
+import { Text, ScrollView, StyleSheet, Image, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Lato_100Thin,
+  Lato_100Thin_Italic,
+  Lato_300Light,
+  Lato_300Light_Italic,
+  Lato_400Regular,
+  Lato_400Regular_Italic,
+  Lato_700Bold,
+  Lato_700Bold_Italic,
+  Lato_900Black,
+  Lato_900Black_Italic,
+} from "@expo-google-fonts/lato";
+
+import {
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
+
+import Skier from "../assets/Skier.png";
 
 const NotificationComponent: FC = () => {
+  let [fontsLoaded] = useFonts({
+    Lato_100Thin,
+    Lato_100Thin_Italic,
+    Lato_300Light,
+    Lato_300Light_Italic,
+    Lato_400Regular,
+    Lato_400Regular_Italic,
+    Lato_700Bold,
+    Lato_700Bold_Italic,
+    Lato_900Black,
+    Lato_900Black_Italic,
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ScrollView style={styles.ScrollStyle}>
-
+      <View style={styles.NotificationView}>
+        <Image source={Skier} style={styles.ImageStyle} />
+        <View style={{ justifyContent: "center" }}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.TextStyle}>Scott Morenzone </Text>
+            <Text style={styles.MiddleTextStyle}>liked</Text>
+            <Text style={styles.TextStyle}> your post</Text>
+          </View>
+          <Text style={styles.TimeText}>8:00 am</Text>
+        </View>
+      </View>
     </ScrollView>
   );
 };
@@ -18,10 +88,27 @@ const styles = StyleSheet.create({
   },
   ScrollStyle: {
     paddingTop: 10,
-    paddingBottom: 5,
   },
   TextStyle: {
-    color: 'white',
+    color: "white",
+    fontSize: 16,
+    fontFamily: "Lato_700Bold",
+  },
+  TimeText: {
+    color: "#DFE6F5",
+    fontSize: 13,
+    paddingTop: 5,
+    fontFamily: "Roboto_500Medium",
+  },
+  NotificationView: {
+    flexDirection: "row",
+    paddingTop: 5,
+    paddingBottom: 34,
+  },
+  MiddleTextStyle: {
+    color: "white",
+    fontSize: 16,
+    fontFamily: "Lato_300Light",
   },
 });
 
--- Screens/NotificationsScreen.tsx
+++ Screens/NotificationsScreen.tsx
@@ -1,7 +1,25 @@
-import { FC } from 'react';
-import { Text, View, StyleSheet, ImageBackground } from 'react-native';
+import { FC } from "react";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import NotificationComponent from "../Components/NotificationComponent";
+import AppLoading from "expo-app-loading";
 import {
-    useFonts,
+  useFonts,
+  Lato_100Thin,
+  Lato_100Thin_Italic,
+  Lato_300Light,
+  Lato_300Light_Italic,
+  Lato_400Regular,
+  Lato_400Regular_Italic,
+  Lato_700Bold,
+  Lato_700Bold_Italic,
+  Lato_900Black,
+  Lato_900Black_Italic,
+} from "@expo-google-fonts/lato";
+
+import CricketPicture from "../assets/Cricket.png";
+
+const NotificationsScreen: FC = () => {
+  let [fontsLoaded] = useFonts({
     Lato_100Thin,
     Lato_100Thin_Italic,
     Lato_300Light,
@@ -12,44 +30,47 @@ import {
     Lato_700Bold_Italic,
     Lato_900Black,
     Lato_900Black_Italic,
-  } from '@expo-google-fonts/lato';
+  });
 
-  import CricketPicture from '../assets/Cricket.png'
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
-const NotificationsScreen: FC = () => {
-    return (
-        <View style={styles.container}>
-            <ImageBackground source={CricketPicture} resizeMode="cover" style={{ height: "100%", width: "100%", backgroundColor: "#0A326D" }}>
-                <View style={styles.overlayContainer}>
-                    
-                    <Text style={styles.NotificationsText}>Notifications</Text>
-                </View>
-                
-            </ImageBackground>
-            
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={CricketPicture}
+        resizeMode="cover"
+        style={{ height: "100%", width: "100%", backgroundColor: "#0A326D" }}
+      >
+        <View style={styles.overlayContainer}>
+          <Text style={styles.NotificationsText}>Notifications</Text>
+          <NotificationComponent />
         </View>
-    )
-}
+      </ImageBackground>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    NotificationsText: {
-        //fontFamily: 'Lato_400Regular',
-        fontStyle: 'normal',
-        fontWeight: '800',
-        color: 'white',
-        fontSize: 32,
-        marginBottom: 5, 
-    },
-    container:{
-        flex: 1,
-        width: "100%",
-        height: "100%",
-    },
-    overlayContainer:{
-        flex: 1,
-        marginTop: 104,
-        marginLeft: 27,
-    },
-})
+  NotificationsText: {
+    fontFamily: "Lato_400Regular",
+    fontStyle: "normal",
+    fontWeight: "800",
+    color: "white",
+    fontSize: 32,
+    marginBottom: 5,
+  },
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  overlayContainer: {
+    flex: 1,
+    marginTop: 104,
+    marginLeft: 27,
+  },
+});
 
 export default NotificationsScreen;
