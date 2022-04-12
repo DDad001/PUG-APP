7f4ae765171b20976b13e3188781e6e96f263176
Jesse <JesseAbarca888@gmail.com>
2022-04-11 20:11:45 -0700

    Fixed the PUG button!

--- Components/PUGButton.tsx
+++ Components/PUGButton.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
-import { View, StyleSheet, Text, Button, Pressable, Image } from "react-native";
-
+import { View, StyleSheet, Text, Pressable } from "react-native";
 
 //You can't style a button have to use a pressable!
 const PUGbutton: FC = () => {
   return (
-      <>
-      <View>
-          <Pressable onPress={() => console.log("Take Me Home!!!!")}>
-          <Text style={styles.BtnBox}>PUG</Text>
-          </Pressable>
+    <>
+      <View style={{ flex: 0.06, marginTop: 50, flexDirection: "row"}}>
+        <Pressable onPress={() => console.log("Take Me Home!!!!")}>
+            <Text style={styles.BtnBox}>PUG</Text>
+        </Pressable>
       </View>
-      </>
+    </>
   );
 };
 
@@ -20,14 +19,14 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     fontSize: 16,
-    backgroundColor: 'rgba(10, 50, 109, 0.1)',
+    backgroundColor: "rgba(10, 50, 109, 0.1)",
     paddingRight: 7,
     paddingTop: 7,
     paddingLeft: 7,
     paddingBottom: 7,
     marginLeft: 10,
     borderWidth: 1,
-    borderColor: 'white'
+    borderColor: "white",
   },
 });
 
