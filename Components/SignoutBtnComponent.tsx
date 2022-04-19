e46f152ee172884d35142a00df6e0a7451a09002
Scotterpop73 <sctzone@aol.com>
2022-04-19 10:26:08 -0700

    Made signout buttton smaller

--- Components/SignoutBtnComponent.tsx
+++ Components/SignoutBtnComponent.tsx
@@ -39,97 +39,155 @@ import {
   Select,
   Text,
   CheckIcon,
-  Box
+  Box,
 } from "native-base";
 
 //You can't style a button have to use a pressable!
 const SignoutBtnComponent: FC = () => {
-    let [fontsLoaded] = useFonts({
-        Lato_100Thin,
-        Lato_100Thin_Italic,
-        Lato_300Light,
-        Lato_300Light_Italic,
-        Lato_400Regular,
-        Lato_400Regular_Italic,
-        Lato_700Bold,
-        Lato_700Bold_Italic,
-        Lato_900Black,
-        Lato_900Black_Italic,
-        Roboto_100Thin,
-        Roboto_100Thin_Italic,
-        Roboto_300Light,
-        Roboto_300Light_Italic,
-        Roboto_400Regular,
-        Roboto_400Regular_Italic,
-        Roboto_500Medium,
-        Roboto_500Medium_Italic,
-        Roboto_700Bold,
-        Roboto_700Bold_Italic,
-        Roboto_900Black,
-        Roboto_900Black_Italic,
-      });
-    
-      // if (!fontsLoaded) {
-      //   return <AppLoading />;
-      // }
+  let [fontsLoaded] = useFonts({
+    Lato_100Thin,
+    Lato_100Thin_Italic,
+    Lato_300Light,
+    Lato_300Light_Italic,
+    Lato_400Regular,
+    Lato_400Regular_Italic,
+    Lato_700Bold,
+    Lato_700Bold_Italic,
+    Lato_900Black,
+    Lato_900Black_Italic,
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
 
-      const [showSignoutModal, setShowSignoutModal] = useState(false);
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // }
+
+  const [showSignoutModal, setShowSignoutModal] = useState(false);
 
   return (
-    
-      <View style={styles.NotificationView}>
-        <Pressable style={{backgroundColor: "#7E90AB", borderRadius: 50, alignItems: "center", marginLeft: 70, marginRight: 70}} onPress={() => setShowSignoutModal(true)} accessibilityLabel="Take Me Home"  >
-            <Text style={{ color: "white",
-    fontFamily: "Roboto_400Regular",
-    fontSize: 24,
-    justifyContent: "center",
-    paddingTop: 18,
-    paddingBottom: 18}}>Sign out</Text>
-        </Pressable>
+    <View style={styles.NotificationView}>
+      <Pressable
+        style={{
+          backgroundColor: "#7E90AB",
+          borderRadius: 50,
+          alignItems: "center", 
+          //justifyContent: "center",
+          marginLeft: 100,
+          marginRight: 100,
+        }}
+        onPress={() => setShowSignoutModal(true)}
+        accessibilityLabel="Take Me Home"
+      >
+        <Text
+          style={{
+            color: "white",
+            fontFamily: "Roboto_400Regular",
+            fontSize: 20,
+            justifyContent: "center",
+            paddingTop: 15,
+            paddingBottom: 15,
+          }}
+        >
+          Sign out
+        </Text>
+      </Pressable>
 
-        <Center style={{ marginTop: 50, flexDirection: 'row' }}>
-      <Modal isOpen={showSignoutModal} onClose={() => setShowSignoutModal(false)}>
-        <Modal.Content maxWidth="400px">
-          <Modal.CloseButton />
-          <Modal.Body style={{ marginLeft: 25, marginRight: 25, marginTop: 20}}>
-            <Text style={{ fontFamily: 'Lato_700Bold', fontSize: 16, fontWeight: '800', color: '#0A326D', textAlign: 'center' }}>Are You Sure You Want To Sign Out?</Text>
-          </Modal.Body>
-              <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: 20}}> 
+      <Center style={{ marginTop: 50, flexDirection: "row" }}>
+        <Modal
+          isOpen={showSignoutModal}
+          onClose={() => setShowSignoutModal(false)}
+        >
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Body
+              style={{ marginLeft: 25, marginRight: 25, marginTop: 20 }}
+            >
+              <Text
+                style={{
+                  fontFamily: "Lato_700Bold",
+                  fontSize: 16,
+                  fontWeight: "800",
+                  color: "#0A326D",
+                  textAlign: "center",
+                }}
+              >
+                Are You Sure You Want To Sign Out?
+              </Text>
+            </Modal.Body>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                marginBottom: 20,
+              }}
+            >
               <Button
-                style={{backgroundColor: '#0A326D', paddingLeft: 30, paddingRight: 30, borderRadius: 10}}
+                style={{
+                  backgroundColor: "#0A326D",
+                  paddingLeft: 30,
+                  paddingRight: 30,
+                  borderRadius: 10,
+                }}
                 onPress={() => {
                   setShowSignoutModal(false);
                 }}
               >
-                <Text style={{ fontFamily: 'Lato_700Bold', fontSize: 16, fontWeight: '800', color: 'white'}}>Yes</Text>
+                <Text
+                  style={{
+                    fontFamily: "Lato_700Bold",
+                    fontSize: 16,
+                    fontWeight: "800",
+                    color: "white",
+                  }}
+                >
+                  Yes
+                </Text>
               </Button>
               <Button
                 onPress={() => {
                   setShowSignoutModal(false);
                 }}
-                style={{backgroundColor: '#0A326D', paddingLeft: 35, paddingRight: 35, borderRadius: 10}}
+                style={{
+                  backgroundColor: "#0A326D",
+                  paddingLeft: 35,
+                  paddingRight: 35,
+                  borderRadius: 10,
+                }}
               >
-                <Text style={{ fontFamily: 'Lato_700Bold', fontSize: 16, fontWeight: '800', color: 'white'}}>No</Text>
+                <Text
+                  style={{
+                    fontFamily: "Lato_700Bold",
+                    fontSize: 16,
+                    fontWeight: "800",
+                    color: "white",
+                  }}
+                >
+                  No
+                </Text>
               </Button>
-              </View>
-            
-             
-            
-          
-        </Modal.Content>
-      </Modal>
-    </Center>
-
-
-      </View>
-    
+            </View>
+          </Modal.Content>
+        </Modal>
+      </Center>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   BtnBox: {
     color: "white",
-    fontFamily: 'Roboto_400Regular',
+    fontFamily: "Roboto_400Regular",
     fontSize: 24,
     paddingRight: 117,
     paddingTop: 24,
@@ -138,13 +196,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#7E90AB",
     borderRadius: 10,
   },
-  Btn: {
-    
-  },
+  Btn: {},
   NotificationView: {
     flex: 1,
     // flexDirection: 'row',
-    justifyContent: 'center'
+    justifyContent: "center",
   },
 });
 
