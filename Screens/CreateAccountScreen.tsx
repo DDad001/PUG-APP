ac4535ef1ba390fe4ae4d8af2c661b04f8217c65
Jesse <JesseAbarca888@gmail.com>
2022-05-25 09:19:45 -0700

    added space between text and input fields

--- Screens/CreateAccountScreen.tsx
+++ Screens/CreateAccountScreen.tsx
@@ -322,7 +322,7 @@ const CreateAccountScreen: FC<Props> = ({navigation}) => {
                 />
                 <Text style={{
                   marginTop: 5,
-                  marginBottom: 10,
+                  marginBottom: 13,
                   color: "white",
                   fontFamily: "Roboto_400Regular",
                   fontSize: 15,
