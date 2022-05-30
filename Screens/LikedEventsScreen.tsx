3a64945a088e72a9a4dc1820713a4464607fa710
Jesse <JesseAbarca888@gmail.com>
2022-05-30 13:00:34 -0700

    Cleaned likedEventsScreen

--- Screens/LikedEventsScreen.tsx
+++ Screens/LikedEventsScreen.tsx
@@ -11,7 +11,6 @@ import AppLoading from "expo-app-loading";
 import { GetLikedEventsByUserId, GetEventItemById, GetIsLiked, AddLikedEvent, DeleteLikedEvent } from "../Services/DataService";
 import UserContext  from '../Context/UserContext';
 
-// Import fonts
 import {
   useFonts,
   Lato_100Thin,
@@ -91,157 +90,6 @@ type RootStackParamList ={
   PastEvents:undefined,
   LikedEvents:undefined
 }
-
-
-
-// const LikedEventsScreen: FC<Props> = ({navigation, route}) => {
-//   const { userItems } = useContext<any>(UserContext);
-  
-//   const [displayEvents, setDisplayEvents] = useState<any>([]);
-  
-
-//   useEffect(() => {
-//     getLikedEventsByUser();
-    
-//   }, []);
-
-//   const getLikedEventsByUser = async () => {
-//     let likedEvents: any[] = [];
-//     let likedEventsIds: number[] = [];
-//     let eventsArr: any[] = [];
-//     let result: any;
-
-//     likedEvents = await GetLikedEventsByUserId(userItems.id);
-    
-//     likedEvents.map(eventObj =>{
-//       likedEventsIds.push(eventObj.eventId)
-//     }) 
-
-//      likedEventsIds.map(async eventId => {
-//       let event: object = await GetEventItemById(eventId);
-//       eventsArr.push(event);
-      
-//     })
-
-//     setTimeout(() => {
-//       setDisplayEvents(eventsArr)
-//     }, 1000)
-    
-//     //setDisplayEvents(result);
-
-    
-//   }
-  
-
-
-//   const handlePastEvents = () => {
-//     navigation.navigate('PastEvents')
-//   }
-//   const handleLikedEvents = () => {
-//     navigation.navigate('LikedEvents')
-//   }
-
-//   let [fontsLoaded, error] = useFonts({
-//     Lato_100Thin,
-//     Lato_100Thin_Italic,
-//     Lato_300Light,
-//     Lato_300Light_Italic,
-//     Lato_400Regular,
-//     Lato_400Regular_Italic,
-//     Lato_700Bold,
-//     Lato_700Bold_Italic,
-//     Lato_900Black,
-//     Lato_900Black_Italic,
-//     Roboto_100Thin,
-//     Roboto_100Thin_Italic,
-//     Roboto_300Light,
-//     Roboto_300Light_Italic,
-//     Roboto_400Regular,
-//     Roboto_400Regular_Italic,
-//     Roboto_500Medium,
-//     Roboto_500Medium_Italic,
-//     Roboto_700Bold,
-//     Roboto_700Bold_Italic,
-//     Roboto_900Black,
-//     Roboto_900Black_Italic,
-//   });
-
-//   if (!fontsLoaded) {
-//     return <AppLoading />;
-//   }
-
-//   return (
-//     <>
-//     <View style={styles.container}>
-//       <ImageBackground source={tennis} resizeMode="cover" style={{ height: "100%", width: "100%", backgroundColor: "#0A326D" }}>
-//       <View style={{flexDirection:'row', justifyContent:'center'}}>
-//               <Pressable onPress={() => navigation.navigate('Nav')}>
-//                 <View style={{backgroundColor:'white', height:35,width:110, marginTop:50, borderTopLeftRadius:10, borderBottomLeftRadius:10,}}>
-//                     <Text style={{marginLeft:20, marginTop:10, fontFamily: "Lato_700Bold",color: "rgba(10, 50, 109, 1)"}}>My Profile</Text>
-//                 </View>
-//               </Pressable>
-//                 <Pressable onPress={handlePastEvents}>    
-//                 <View style={{backgroundColor:'white', height:35,width:110, marginTop:50}}>
-//                     <Text style={{marginLeft:12, marginTop:10, fontFamily: "Lato_700Bold",color: "rgba(10, 50, 109, 1)"}}>Past events</Text>
-//                 </View>
-//                 </Pressable>
-//                 <Pressable onPress={handleLikedEvents}>
-//                 <View style={{backgroundColor:'white', height:35,width:110, marginTop:50,borderTopRightRadius:10, borderBottomRightRadius:10}}>
-//                     <Text style={{marginLeft:10, marginTop:10, fontFamily: "Lato_700Bold",color: "rgba(10, 50, 109, 1)"}}>Liked events</Text>
-//                 </View>
-//                 </Pressable>
-//              </View>
-
-//                  <Text style={{marginLeft:22, marginTop:50,color:'white', fontSize:35, fontFamily: "Lato_700Bold", fontWeight: "bold",}}>Liked Events</Text>
-//                  <ScrollView>
-//                 {
-//                   displayEvents.map((likedEvent: any, idx: number) => {
-//                     return (
-                    
-//                                  <View style={styles.card} key={idx}>
-//                                   <View style={styles.cardContent}>
-//                                   <View style={{ flexDirection: 'row', }}>
-//                                   <Image source={man} style={{ height: 100, width: 145, borderRadius: 8 }} />
-//                                   <View style={{marginLeft:35}}>
-//                                     <View style={{ flexDirection: 'row' }}>
-//                                       <MaterialCommunityIcons name="calendar-month" size={23} color="rgba(10, 50, 109, 1)" style={{ marginTop: 6, marginLeft: 14}} />
-//                                         <Text style={{ marginTop: 9, marginLeft: 5, fontFamily: "Roboto_400Regular", fontSize: 16 }}>{likedEvent.dateOfEvent}</Text>
-//                                     </View>
-//                                     <View style={{ flexDirection: 'column', }}>
-//                                       <View style={{ flexDirection: 'row', }}>
-//                                       <MaterialCommunityIcons name="clock-time-three-outline" size={23} color="rgba(10, 50, 109, 1)" style={{ marginTop:5, marginLeft: 14 }} />
-//                                         <Text style={{ marginTop: 8, marginLeft: 5, fontFamily: "Roboto_400Regular", fontSize: 16  }}>{likedEvent.timeOfEvent}</Text>
-//                                       </View>
-
-//                                       <View style={{ flexDirection: 'row' }}>
-//                                         <Text style={{fontSize:12, marginLeft:20, marginTop:8, fontFamily: "Lato_400Regular"}}>{likedEvent.addressOfEvent}</Text>
-//                                       </View>
-
-
-//                                         </View>
-//                                       </View>
-//                                     </View>
-//                                       <Text style={{marginLeft:4, marginTop:5, fontFamily: "Lato_700Bold", fontSize:14}}>{likedEvent.nameOfEvent}</Text>
-
-
-//                                       <View style={{flexDirection:'row', justifyContent:'flex-end'}}>
-//                                       <MaterialIcons name="location-on" size={16} color="white" style={{ backgroundColor: '#0A326D', borderRadius: 3, overflow:'hidden', marginLeft: 12, padding:5  }} />
-//                                       <FontAwesome5 name="heart" size={13} color="white" style={{ backgroundColor: '#0A326D', borderRadius: 3, overflow:'hidden', padding:6.5,marginLeft:9 }} />
-//                                       </View>
-
-//                                   </View>
-//                                 </View>
-                    
-//                     )
-//                   })
-//                 }
-//         </ScrollView>
-//       </ImageBackground>
-//     </View> 
-//     </>
-//   );
-// };
-
 
 const LikedEventItems = ({ id, dateOfEvent,timeOfEvent,addressOfEvent,nameOfEvent, getLikedEventsByUser, navigation, sportOfEvent }: any) => {
   const { userItems, setUpdateScreen, setUpdateProfileScreen } = useContext<any>(UserContext);
@@ -328,7 +176,6 @@ const LikedEventItems = ({ id, dateOfEvent,timeOfEvent,addressOfEvent,nameOfEven
                                                           <Image source={pugEvent} style={{ height: 100, width: 145, borderRadius: 8 }} />
             }
 
-                                  {/* <Image source={man} style={{ height: 100, width: 145, borderRadius: 8 }} /> */}
                                   <View style={{marginLeft:35}}>
                                     <View style={{ flexDirection: 'row' }}>
                                       <MaterialCommunityIcons name="calendar-month" size={23} color="rgba(10, 50, 109, 1)" style={{ marginTop: 6, marginLeft: 14}} />
@@ -390,7 +237,6 @@ const LikedEventItems = ({ id, dateOfEvent,timeOfEvent,addressOfEvent,nameOfEven
   }, []);
 
   const getLikedEventsByUser = async () => {
-    //let likedEvents: any[] = [];
     let likedEventsIds: number[] = [];
     let eventsArr: object[] = [];
 
@@ -403,9 +249,6 @@ const LikedEventItems = ({ id, dateOfEvent,timeOfEvent,addressOfEvent,nameOfEven
         setDisplayEvents(eventsArr)
       }
     }) 
-
-    //setDisplayEvents(result);
-
     
   }
 
@@ -420,15 +263,7 @@ const LikedEventItems = ({ id, dateOfEvent,timeOfEvent,addressOfEvent,nameOfEven
       getLikedEventsByUser();
     }, 1000)
     
-  }
-
-  // const checkIfLiked = async (eventId: number) => {
-  //   let liked = await GetIsLiked(userItems.id, eventId);
-    
-  //   setIsLiked(liked);
-  //   //console.log(liked);
-  // }
-  
+  }  
   
   const renderItem = ({ item }: any) => {
     return (
